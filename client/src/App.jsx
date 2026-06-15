fc9ad743c086ced9a723a299b1ef9e591c2550cd
Akshada Pal <akshadapal2020@gmail.com>
2026-06-15 17:13:42 +0530

    connect frontend to render backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import Addproperty from "./pages/AddProperty";
+import Addproperty from "./pages/Addproperty";
 import Properties from "./pages/Properties";
 import Profile from "./pages/Profile";
 import ProtectedRoute from "./components/ProtectedRoute";
